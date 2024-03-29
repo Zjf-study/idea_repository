2cea860cd46cff03f02ee3adc0a1352b4947b88e
Zjf-study <135221269+Zjf-study@users.noreply.github.com>
2024-03-29 21:39:57 +0800

    Update Test.java

--- src/MyIOproject/Test.java
+++ src/MyIOproject/Test.java
@@ -4,5 +4,6 @@ package MyIOproject;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello world!");
+         System.out.println("-----------------------");
     }
 }
